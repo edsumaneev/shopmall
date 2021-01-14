9e7d31a91990822815c9518f9c7834b141c6bb5d
edsumaneev <edsumaneev@yandex.ru>
2021-01-14 13:07:08 +0300

    + 2 popup 2020-01-14

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -59,14 +59,12 @@ require('magnific-popup');
 // Magnific Popup
 $('.show-popup').magnificPopup({
 type: 'inline',
-removalDelay: 500,
+removalDelay: 300,
 callbacks: {
   beforeOpen: function () {
     this.st.mainClass = this.st.el.attr('data-effect');
+  }  
   }
-}
-// ,
-// midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
 // END Magnific Popup
 })();
